f7aa5f4ca8b264c28558c1a5e9551c8dabc0ece7
sanika <sanikagoyal9@gmail.com>
2022-10-03 15:22:38 +0530

    Rsponsive Close Envelope done

--- animation.js
+++ animation.js
@@ -1,9 +1,14 @@
 /*WORKING OF CLOSED ENVELOPE*/
 let closeEnvelope = document.querySelector("#closeEnvelope");
+var responsive = window.matchMedia("(max-width:1000px)");
 
-window.onload = () => {
-  animatedCloseLetter();
-};
+function MyFunction1(responsive) {
+  if (responsive.matches) {
+    responsiveCloseLetter();
+  } else {
+    animatedCloseLetter();
+  }
+}
 
 function animatedCloseLetter() {
   let maxPosition = 300;
@@ -14,7 +19,22 @@ function animatedCloseLetter() {
     if (position > maxPosition || position < 0) {
       speed = speed * -1;
     }
-    //   speed++;
     closeEnvelope.style.left = position + "px";
   }, 1000 / 40);
 }
+
+function responsiveCloseLetter() {
+  let maxPosition = 51;
+  let speed = 1;
+  let position = 0;
+  const animation = setInterval(() => {
+    position = position + speed;
+    if (position > maxPosition || position < 0) {
+      speed = speed * -1;
+    }
+    closeEnvelope.style.left = position + "px";
+  }, 1000 / 40);
+}
+
+MyFunction1(responsive);
+responsive.addEventListener(MyFunction1);
